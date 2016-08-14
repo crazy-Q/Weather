e6caea266b38e62f4d5e3c16e7c5c613f6eb469e
crazy-Q <qmx754@163.com>
2016-08-14 21:20:28 +0800

    完成Weather界面UI设计

--- app/src/main/java/com/jikexueyuan/weather/WeatherActivity.java
+++ app/src/main/java/com/jikexueyuan/weather/WeatherActivity.java
@@ -1,14 +1,15 @@
 package com.jikexueyuan.weather;
 
+import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.ListView;
+import android.widget.TextView;
 
 public class WeatherActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
-    private SwipeRefreshLayout swipeRefreshLayout;
-    private ListView listView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private TextView tvCity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +24,11 @@ public class WeatherActivity extends AppCompatActivity implements SwipeRefreshLa
     }
 
     private void initView() {
-//        listView = (ListView) findViewById(R.id.list);
-        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.id_swipe_refresh_container);
-        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_purple,
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.id_swipe_refresh_container);
+        mSwipeRefreshLayout.setColorSchemeResources(android.R.color.holo_purple,
                 android.R.color.holo_blue_bright,
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
-        swipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
     }
 }
